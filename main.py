aab210d9dd5a01b7e169f2e09051451168786be4
gachabe <gchavesbenitez@gmail.com>
2023-06-21 16:07:07 +0200

    Nueva versión del proyecto final

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # PROYECTO FINAL GABRIEL CHAVES BENITEZ; CURSO E23
+
+from cargar_datos import cargar_datos_bbdd as bbdd
+
+conexion = bbdd.conexion_bbdd("bbdd/videojuegos.db")
+
+id_red_social = bbdd.buscar_red_social(conexion, "Playstore")
+print("Red social: ",id_red_social)
+
+id_red_social = bbdd.insertar_red_social(conexion,"Twitter","Twitter.com")
+print("Rdo social  nueva: ", id_red_social)
