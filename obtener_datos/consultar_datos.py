268bd1f05dc83bc028a04a869d6e39d1a083a038
gachabe <gchavesbenitez@gmail.com>
2023-06-28 13:46:28 +0200

    Final clase martes - arreglado

--- obtener_datos/consultar_datos.py
+++ obtener_datos/consultar_datos.py
@@ -1,5 +1,23 @@
 import pandas as pd
 def consultar_comentarios_fecha(conexion, palabra_clave,f_inicio,f_fin):
-    query = "SELECT * FROM mensaje WHERE text_mensaje like"
+    query = "SELECT nick_usuario" \
+            " FROM usuario " \
+            "WHERE id_usuario in" \
+            "(SELECT id_usuario  FROM mensaje" \
+            "WHERE text_mensaje like '%{}%' " \
+            "AND f_mensaje >=  DATE('{}')" \
+            "AND f_mensaje <= DATE('{}'))".format(palabra_clave,f_inicio,f_fin)
+
+    df = pd.read_sql_query(query,conexion)
+    print(df)
+
+def consultar_comentarios_cantidad(conexion, palabra_clave):
+    query = "SELECT usuario.nick_usuario, count(mensaje.text_mensaje) as cantidad" \
+            "FROM mensaje " \
+            "INNER JOIN  usuario ON usuario.id_usuario = mensaje.id_usuario" \
+            "GROUP BY mensaje.id_usuario" \
+            "HAVING text_mensaje like '%{}%' " \
+            "ORDER  BY cantidad DESC ".format(palabra_clave)
+
     df = pd.read_sql_query(query,conexion)
     print(df)
